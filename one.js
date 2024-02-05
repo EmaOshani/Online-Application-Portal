0f880ea617d44da864e64362ce680266e8bb2d79
EmaOshani <imashasamarasekara99@gmail.com>
2024-02-05 23:41:18 +0530

    first commit

---
+++ one.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var Link = document.getElementById("link");
+
+    Link.addEventListener("click", function() {
+        window.location.href = "two.html";
+    });
+});
